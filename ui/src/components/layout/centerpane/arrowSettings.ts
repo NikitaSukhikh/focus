d84493ab4d9e5739b61a78eb539ec4dbc6275ccb
Nikita Sukhikh <nixirom@gmail.com>
2025-12-28 10:57:46 +0000

    Arrow restyling and refactoring

---
+++ ui/src/components/layout/centerpane/arrowSettings.ts
@@ -0,0 +1,17 @@
+export const ARROW_SETTINGS = {
+  color: '#FFEB3B',
+  strokeWidth: 3,
+  clickAreaPadding: 6,
+  opacity: {
+    normal: 1,
+    draft: 0.5,
+  },
+  marker: {
+    id: 'center-pane-arrowhead',
+    width: 12,
+    height: 12,
+    refX: 10,
+    refY: 6,
+    path: 'M0 0 L12 6 L0 12 L3 6 Z',
+  },
+} as const;
