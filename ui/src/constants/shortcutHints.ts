278c23cf77b1c357b2312740bae81eeb16e04398
Nikita Sukhikh <nixirom@gmail.com>
2026-01-03 07:33:17 +0000

    Replacing 'Ctrl+L' with 'Ctrl+L'

--- ui/src/constants/shortcutHints.ts
+++ ui/src/constants/shortcutHints.ts
@@ -1,7 +1,7 @@
 export const SHORTCUT_HINT_LINES = [
   "Press 'Ctrl+Y' to create new space",
   "'Ctrl+I' or right click - add link/files",
-  "'Ctrl+L' - toggle left side bar",
+  "'Ctrl+Left' - toggle left side bar",
   "'Ctrl+U' - toggle preview pane",
 ] as const;
 
